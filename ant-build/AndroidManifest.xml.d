c5e715438aa146dff37f15a80aaa049d0931b761
dayan <dayan@getconsultoria.com.br>
2015-02-25 10:33:56 -0300

    first commit

--- ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\ant-build\AndroidManifest.xml : \
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\AndroidManifest.xml \
--- ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\ant-build\CordovaApp.ap_ \
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\ant-build\classes.dex \
-C:\powerget\jaguar\workspace\pedidoweb_parent\pedidomobile\platforms\android\CordovaLib\ant-build\classes.jar \
