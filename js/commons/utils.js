6365f7544895df976ebe97e49ba14687e7ef20d7
dayan <dayan@getconsultoria.com.br>
2015-02-26 08:13:31 -0300

    first commit

--- js/commons/utils.js
+++
@@ -1,9 +0,0 @@
-var db1 = window.openDatabase(nomeBanco, versaoBanco, "", 1000000);
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-		if ((new Date().getTime() - start) > milliseconds){
-			break;
-		}
-  }
-}
